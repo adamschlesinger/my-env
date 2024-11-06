f26040c471c30dfbda69239118bd2feef7b8e8ab
Adam Schlesinger <adam.d.schlesinger@gmail.com>
2024-11-05 21:03:59 -0500

    Synced

--- sync.sh
+++ sync.sh
@@ -4,12 +4,18 @@ script_path=$(readlink -f "$0")
 script_dir=$(dirname "$script_path")
 files_dir="$script_dir/files"
 
+# home dotfiles
 cp "$HOME/.alacritty.toml" "$files_dir"
 cp "$HOME/.gitconfig" "$files_dir"
 cp "$HOME/.p10k.zsh" "$files_dir"
 cp "$HOME/.tmux.conf" "$files_dir"
 cp "$HOME/.zshrc" "$files_dir"
 
+# lsd
 cp "$HOME/.config/lsd/config.yaml" "$file_dir/lsd_config.yaml"
 
+# vim
 cp "$HOME/.vim_runtime/my_configs.vim" "$files_dir"
+
+# tmux
+cp -R "$HOME/.config/tmux-powerline" "$file_dir"
